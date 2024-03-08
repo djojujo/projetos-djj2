3037c05580516423af552ca7b1d7efb1ab1b0bfe
djojujo <159132703+djojujo@users.noreply.github.com>
2024-03-08 12:15:50 -0300

    .

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-var respostas = '';
-var texto = document.querySelector('span');
+var respostas = ''; //variavel da resposta que o site pergunta//
+var texto = document.querySelector('span');//variavel aonde eu consigo achar o span//
 
 while(respostas == ''){ // || respostas == null
     respostas = prompt('Você gosta de golfinho?');
-}
+}//enquanto o resposta estiver nulo ou quando tiver uma resposta//
 
 if(respostas == null){
 texto.textContent = 'não sei'
 }else{
 texto.textContent = respostas;
-}
+}//e se for nulo a resposta 'não sei' aparecera//
+//serve para colocar o site de folga para o site funcionar//
