9ef7e1e426e5888b685b3e4fe6e2a0f020397975
djojujo <159132703+djojujo@users.noreply.github.com>
2024-03-11 11:50:03 -0300

    .

--- jogo.js
+++ jogo.js
@@ -1,3 +1,10 @@
 let numeroSecreto = 19;
 
-prompt('Escola um numero de 0 a 20');
+let chute = prompt('Escola um numero de 0 a 20');
+
+if(numeroSecreto == chute){
+alert('Parabens,voce acertou!!!!');
+}else{
+    alert('Que triste, você errou :(');
+}
+    
