0400e773f8b7ad63a8d568ec5a0c3aa4c14e992b
djojujo <159132703+djojujo@users.noreply.github.com>
2024-04-11 08:52:55 -0300

    ;

--- cont.js
+++ cont.js
@@ -5,6 +5,8 @@ console.log(botoes.length);
 
 for(let djojujo = 0; djojujo<botoes.length; djojujo++) {
 console.log(djojujo);
+botoes[djojujo].onclick = function(){
 botoes[djojujo].classList.add('ativo');
 //botoes[0].classList.remove('ativo');
+}
 }
