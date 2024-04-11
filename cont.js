9f64c69b97698a160c16e89ab40473fbd41707d1
djojujo <159132703+djojujo@users.noreply.github.com>
2024-04-11 09:02:33 -0300

    ,

--- cont.js
+++ cont.js
@@ -6,7 +6,9 @@ console.log(botoes.length);
 for(let djojujo = 0; djojujo<botoes.length; djojujo++) {
 console.log(djojujo);
 botoes[djojujo].onclick = function(){
+    for(let djj = 0; djj<botoes.length; djj++) {
+        botoes[djj].classList.remove('ativo');
+    }
 botoes[djojujo].classList.add('ativo');
-//botoes[0].classList.remove('ativo');
 }
 }
