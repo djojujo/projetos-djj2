91b32bef3d02643474b2cd7c67780fe556dab94d
djojujo <159132703+djojujo@users.noreply.github.com>
2024-04-11 09:24:08 -0300

    ,

--- cont.js
+++ cont.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll('.objetivos');
-console.log(botoes.length);
+const textos = document.querySelectorAll('.aba-conteudo');
 
 
 
@@ -8,7 +8,9 @@ console.log(djojujo);
 botoes[djojujo].onclick = function(){
     for(let djj = 0; djj<botoes.length; djj++) {
         botoes[djj].classList.remove('ativo');
+        textos[djj].classList.remove('ativa');
     }
 botoes[djojujo].classList.add('ativo');
+textos[djj].classList.add('ativa');
 }
 }
