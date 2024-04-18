04cc80e834d16572fbfe2eda96cd22ca88a63f5c
djojujo <159132703+djojujo@users.noreply.github.com>
2024-04-18 10:21:06 -0300

    .

--- cont.js
+++ cont.js
@@ -13,8 +13,11 @@ botoes[djojujo].classList.add('ativo');
 textos[djojujo].classList.add('ativa');
 }
 }
+
 const contadores = document.querySelectorAll('.contador');
 let tempoAtual =new Date();
 let tempoObjetivo1 = new Date ('2024-04-19:00:00');
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = tempoFinal/1000;
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = ;
