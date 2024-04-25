6136407a45aed3ee7cb799f3ef73edc1dc32b21c
djojujo <159132703+djojujo@users.noreply.github.com>
2024-04-25 09:16:00 -0300

    m

--- cont.js
+++ cont.js
@@ -20,6 +20,10 @@ let tempoObjetivo1 = new Date ('2024-04-30T00:00:00');
 let tempoObjetivo2 = new Date ('2024-05-30T00:00:00');
 let tempoObjetivo3 = new Date ('2024-06-30T00:00:00');
 let tempoObjetivo4 = new Date ('2024-12-13T00:00:00');
+
+let tempos = [tempoObjetivo1. tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function(){
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 let segundos = Math.floor (tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
@@ -31,3 +35,4 @@ minutos %= 60;
 horas %= 24;
 
 contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
