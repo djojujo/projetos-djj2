c3a28fd58f4aeaa822858105a496c27c0fa7b611
djojujo <159132703+djojujo@users.noreply.github.com>
2024-04-25 09:18:18 -0300

    .

--- cont.js
+++ cont.js
@@ -23,7 +23,7 @@ let tempoObjetivo4 = new Date ('2024-12-13T00:00:00');
 
 let tempos = [tempoObjetivo1. tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function(){
+function CalculaTempo(tempoObjetivo){
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 let segundos = Math.floor (tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
