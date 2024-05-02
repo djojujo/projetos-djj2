a36d258f8f0474ddd45a0afc65c77a9f9d578c76
djojujo <159132703+djojujo@users.noreply.github.com>
2024-05-02 10:20:33 -0300

    .

--- cont.js
+++ cont.js
@@ -15,10 +15,10 @@ for (let djojujo = 0; djojujo < botoes.length; djojujo++) {
 
 const contadores = document.querySelectorAll('.contador');
 let tempoAtual = new Date();
-let tempoObjetivo1 = new Date('2024-04-30T00:00:00');
-let tempoObjetivo2 = new Date('2024-05-30T00:00:00');
-let tempoObjetivo3 = new Date('2024-06-30T00:00:00');
-let tempoObjetivo4 = new Date('2024-12-13T00:00:00');
+let tempoObjetivo1 = new Date('2025-02-30T00:00:00');
+let tempoObjetivo2 = new Date('2025-05-30T00:00:00');
+let tempoObjetivo3 = new Date('2025-06-30T00:00:00');
+let tempoObjetivo4 = new Date('2025-12-13T00:00:00');
 
 let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
